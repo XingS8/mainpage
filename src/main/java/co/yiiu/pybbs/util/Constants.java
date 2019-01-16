244a3a9876755a5f31f6fd35358711ade09b519b
xingshen <345761836@qq.com>
2019-01-16 15:30:46 +0800

    模板

---
+++ src/main/java/co/yiiu/pybbs/util/Constants.java
@@ -0,0 +1,11 @@
+package co.yiiu.pybbs.util;
+
+/**
+ * Created by tomoya.
+ * Copyright (c) 2018, All Rights Reserved.
+ * https://yiiu.co
+ */
+public class Constants {
+
+  public static final String REDIS_SYSTEM_CONFIG_KEY = "system_config";
+}
